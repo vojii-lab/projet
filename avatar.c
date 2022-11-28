b44ba65a29123e3a6766b708a8e094cc663a0d9e
vojii-lab <114113211+vojii-lab@users.noreply.github.com>
2022-11-28 13:37:40 -0500

    Update avatar.c

--- avatar.c
+++ avatar.c
@@ -36,129 +36,162 @@ void drawAvatar(struct Avatar *avatar){
 	int color = avatar->color;
 	int step = avatar->step;
 
-	
-	
-	if(step%2 == 0){
 	// DELETE PREVIOUS
-		LCD_FillRect(posx, previous_posy, width, length, COLOR_BACKGROUND);
+	LCD_FillRect(posx, previous_posy, width+1, length, COLOR_BACKGROUND);
 	
 	// PRINT NEW
-	//Queue
-	LCD_DrawLine(posx,posy+15,posx,posy+21,color);
-	LCD_DrawLine(posx+1,posy+18,posx+1,posy+22,color);
-	LCD_DrawLine(posx+2,posy+20,posx+2,posy+23,color);
-	LCD_DrawLine(posx+3,posy+21,posx+3,posy+23,color);
-	LCD_DrawLine(posx+4,posy+22,posx+4,posy+24,color);
-	LCD_DrawLine(posx+5,posy+22,posx+5,posy+24,color);
-	
-	//Corps
-	LCD_DrawLine(posx+6,posy+20,posx+6,posy+23,color);
-	LCD_DrawLine(posx+7,posy+18,posx+7,posy+24,color);
-	LCD_DrawLine(posx+8,posy+17,posx+8,posy+24,color);
-	LCD_DrawLine(posx+9,posy+16,posx+9,posy+29,color);
-	LCD_DrawLine(posx+10,posy+15,posx+10,posy+29,color);
-	LCD_DrawLine(posx+11,posy+15,posx+11,posy+26,color);
-	LCD_DrawLine(posx+12,posy+14,posx+12,posy+25,color);
-	LCD_DrawLine(posx+13,posy+13,posx+13,posy+23,color);
-	LCD_DrawLine(posx+14,posy+12,posx+14,posy+23,color);
-	LCD_DrawLine(posx+15,posy+1,posx+15,posy+23,color);
-	LCD_DrawLine(posx+16,posy+1,posx+16,posy+25,color);
-	LCD_DrawLine(posx+17,posy+0,posx+17,posy+25,color);
-	LCD_DrawLine(posx+18,posy+0,posx+18,posy+26,color);
-	LCD_DrawLine(posx+19,posy+0,posx+19,posy+28,color);
-	LCD_DrawLine(posx+20,posy+0,posx+20,posy+28,color);
-
-	// Tête
-	LCD_DrawLine(posx+19,posy+3,posx+19,posy+4,COLOR_BACKGROUND); //oeil
-	LCD_DrawLine(posx+20,posy+3,posx+20,posy+4,COLOR_BACKGROUND); //oeil
-	LCD_DrawLine(posx+21,posy+0,posx+21,posy+20,color);
-	LCD_DrawLine(posx+22,posy+0,posx+22,posy+10,color);
-	LCD_DrawLine(posx+23,posy+0,posx+23,posy+10,color);
-	LCD_DrawLine(posx+24,posy+0,posx+24,posy+7,color);
-	LCD_DrawLine(posx+25,posy+0,posx+25,posy+7,color);
-	LCD_DrawLine(posx+26,posy+0,posx+26,posy+7,color);
-	LCD_DrawLine(posx+27,posy+1,posx+27,posy+7,color);
-	LCD_DrawLine(posx+28,posy+1,posx+28,posy+7,color);
-	LCD_DrawLine(posx+29,posy+1,posx+29,posy+7,color);
-	
-	// Bouche
-	LCD_DrawLine(posx+24,posy+9,posx+24,posy+10,color);
-	LCD_DrawLine(posx+25,posy+9,posx+25,posy+10,color);
-	LCD_DrawLine(posx+26,posy+9,posx+26,posy+10,color);
-	LCD_DrawLine(posx+27,posy+9,posx+27,posy+10,color);
-	
-	//Pied
-	LCD_DrawLine(posx+11,posy+28,posx+11,posy+29,color);
-	LCD_DrawLine(posx+12,posy+28,posx+12,posy+29,color);
-	LCD_DrawLine(posx+21,posy+27,posx+19,posy+28,color);
-	LCD_DrawLine(posx+22,posy+27,posx+20,posy+28,color);
-	
-	//Bras
-	LCD_DrawLine(posx+22,posy+15,posx+22,posy+16,color);
-	LCD_DrawLine(posx+23,posy+15,posx+23,posy+17,color);
-	LCD_DrawLine(posx+24,posy+15,posx+24,posy+17,color);
-	}
-	else {
-	// DELETE PREVIOUS
-	LCD_FillRect(posx, previous_posy, width, length, COLOR_BACKGROUND);
-	
-	// PRINT NEW
-	//Queue
-
-	LCD_DrawLine(posx+2,posy+18,posx+2,posy+21,color);
-	LCD_DrawLine(posx+3,posy+15,posx+3,posy+22,color);
-	LCD_DrawLine(posx+4,posy+14,posx+4,posy+23,color);
-	LCD_DrawLine(posx+5,posy+13,posx+5,posy+15,color);
-	LCD_DrawLine(posx+5,posy+19,posx+5,posy+23,color);
-	LCD_DrawLine(posx+6,posy+20,posx+6,posy+23,color);
-	LCD_DrawLine(posx+6,posy+12,posx+6,posy+12,color);
-	
-	//Corps
-	LCD_DrawLine(posx+7,posy+18,posx+7,posy+24,color);
-	LCD_DrawLine(posx+8,posy+17,posx+8,posy+24,color);
-	LCD_DrawLine(posx+9,posy+16,posx+9,posy+26,color);
-	LCD_DrawLine(posx+10,posy+15,posx+10,posy+27,color);
-	LCD_DrawLine(posx+11,posy+15,posx+11,posy+28,color);
-	LCD_DrawLine(posx+12,posy+14,posx+12,posy+25,color);
-	LCD_DrawLine(posx+13,posy+13,posx+13,posy+23,color);
-	LCD_DrawLine(posx+14,posy+12,posx+14,posy+23,color);
-	LCD_DrawLine(posx+15,posy+1,posx+15,posy+25,color);
-	LCD_DrawLine(posx+16,posy+1,posx+16,posy+27,color);
-	LCD_DrawLine(posx+17,posy+0,posx+17,posy+29,color);
-	LCD_DrawLine(posx+18,posy+0,posx+18,posy+29,color);
-	LCD_DrawLine(posx+19,posy+0,posx+19,posy+24,color);
-	LCD_DrawLine(posx+20,posy+0,posx+20,posy+23,color);
-
-	// Tête
-	LCD_DrawLine(posx+19,posy+3,posx+19,posy+4,COLOR_BACKGROUND); //oeil
-	LCD_DrawLine(posx+20,posy+3,posx+20,posy+4,COLOR_BACKGROUND); //oeil
-	LCD_DrawLine(posx+21,posy+0,posx+21,posy+20,color);
-	LCD_DrawLine(posx+22,posy+0,posx+22,posy+15,color);
-	LCD_DrawLine(posx+23,posy+0,posx+23,posy+10,color);
-	LCD_DrawLine(posx+24,posy+0,posx+24,posy+7,color);
-	LCD_DrawLine(posx+25,posy+0,posx+25,posy+7,color);
-	LCD_DrawLine(posx+26,posy+0,posx+26,posy+7,color);
-	LCD_DrawLine(posx+27,posy+1,posx+27,posy+7,color);
-	LCD_DrawLine(posx+28,posy+1,posx+28,posy+7,color);
-	LCD_DrawLine(posx+29,posy+1,posx+29,posy+7,color);
-	
-	// Bouche
-	LCD_DrawLine(posx+24,posy+9,posx+24,posy+10,color);
-	LCD_DrawLine(posx+25,posy+9,posx+25,posy+10,color);
-	LCD_DrawLine(posx+26,posy+9,posx+26,posy+10,color);
-	LCD_DrawLine(posx+27,posy+9,posx+27,posy+10,color);
-	
-	//Pied
-	LCD_DrawLine(posx+11,posy+28,posx+11,posy+29,color);
-	LCD_DrawLine(posx+12,posy+28,posx+12,posy+29,color);
-	LCD_DrawLine(posx+19,posy+28,posx+19,posy+29,color);
-	LCD_DrawLine(posx+20,posy+28,posx+20,posy+29,color);
-	
-	//Bras
-	LCD_DrawLine(posx+23,posy+14,posx+23,posy+16,color);
-	LCD_DrawLine(posx+24,posy+14,posx+24,posy+16,color);
-	}
+	LCD_DrawLine(posx+9,posy+11,posx+9,posy+19, color);
+		
+	LCD_DrawLine(posx+10,posy+3,posx+10,posy+4,color);	
+	LCD_DrawLine(posx+10,posy+10,posx+10,posy+12,color);
+	LCD_DrawLine(posx+10,posy+28,posx+10,posy+28,color);
+		
+	LCD_DrawLine(posx+11,posy+4,posx+11,posy+4,color);	
+	LCD_DrawLine(posx+11,posy+10,posx+11,posy+28,color);
+		
+	LCD_DrawLine(posx+12,posy+3,posx+12,posy+28,color);
+		
+	LCD_DrawLine(posx+13,posy+2,posx+13,posy+21,color);
+		
+	LCD_DrawLine(posx+14,posy+1,posx+14,posy+20,color);
+		
+		
+	LCD_DrawLine(posx+20,posy+11,posx+20,posy+19, color);
+		
+	LCD_DrawLine(posx+19,posy+3,posx+19,posy+4,color);	
+	LCD_DrawLine(posx+19,posy+10,posx+19,posy+12,color);
+	LCD_DrawLine(posx+19,posy+28,posx+19,posy+28,color);
+		
+	LCD_DrawLine(posx+18,posy+4,posx+18,posy+4,color);	
+	LCD_DrawLine(posx+18,posy+10,posx+18,posy+28,color);
+		
+	LCD_DrawLine(posx+17,posy+3,posx+17,posy+28,color);
+		
+	LCD_DrawLine(posx+16,posy+2,posx+16,posy+21,color);
+		
+	LCD_DrawLine(posx+15,posy+1,posx+15,posy+20,color);	
 }
+
+void drawDino(struct Obstacle *obstacle){
+	
+	obstacle->color=WHITE;
+	
+	int posy = obstacle->posy;
+	int id = obstacle->idTroupe;
+	int posx = obstacle->posx;
+	int color = obstacle->color;
+	
+	if (id == 1){
+		drawDinos(posx, posy, color);
+	}
+	
+	if (id == 2){
+		drawDinos(posx, posy, color);
+		drawDinos(posx+30, posy+30, color);
+	}
+	
+	if (id == 3){
+		drawDinos(posx, posy+60, color);
+		drawDinos(posx+30, posy, color);
+		drawDinos(posx+60, posy+30, color);
+	}
+	
+	if (id == 4){
+		drawDinos(posx, posy, color);
+		drawDinos(posx+30, posy+30, color);
+		drawDinos(posx, posy+60, color);
+	}
+	
+	if (id == 5){
+		drawDinos(posx, posy+60, color);
+		drawDinos(posx+30, posy, color);
+		drawDinos(posx+60, posy+30, color);
+		drawDinos(posx+60, posy+60, color);
+	}
+	
+	if (id == 6){
+		drawDinos(posx, posy+30, color);
+		drawDinos(posx+30, posy, color);
+		drawDinos(posx+30, posy+60, color);
+		drawDinos(posx+60, posy+30, color);
+	}
+	
+	if (id == 7){
+		drawDinos(posx, posy+30, color);
+		drawDinos(posx+30, posy+60, color);
+		drawDinos(posx+60, posy+60, color);
+	}
+	
+	if (id == 8){
+		drawDinos(posx, posy+60, color);
+		drawDinos(posx+30, posy+60, color);
+		drawDinos(posx+30, posy+30, color);
+		drawDinos(posx+60, posy, color);
+	}
+	
+	obstacle->length = 90;
+	obstacle->width = 90;
+}
+
+void drawDinos(posx, posy, color){
+		
+	//Queue
+	LCD_DrawLine(29+posx,posy+15,29+posx,posy+21,color);
+	LCD_DrawLine(29+posx-1,posy+18,29+posx-1,posy+22,color);
+	LCD_DrawLine(29+posx-2,posy+20,29+posx-2,posy+23,color);
+	LCD_DrawLine(29+posx-3,posy+21,29+posx-3,posy+23,color);
+	LCD_DrawLine(29+posx-4,posy+22,29+posx-4,posy+24,color);
+	LCD_DrawLine(29+posx-5,posy+22,29+posx-5,posy+24,color);
+	
+	//Corps
+	LCD_DrawLine(29+posx-6,posy+20,29+posx-6,posy+23,color);
+	LCD_DrawLine(29+posx-7,posy+18,29+posx-7,posy+24,color);
+	LCD_DrawLine(29+posx-8,posy+17,29+posx-8,posy+24,color);
+	LCD_DrawLine(29+posx-9,posy+16,29+posx-9,posy+29,color);
+	LCD_DrawLine(29+posx-10,posy+15,29+posx-10,posy+29,color);
+	LCD_DrawLine(29+posx-11,posy+15,29+posx-11,posy+26,color);
+	LCD_DrawLine(29+posx-12,posy+14,29+posx-12,posy+25,color);
+	LCD_DrawLine(29+posx-13,posy+13,29+posx-13,posy+23,color);
+	LCD_DrawLine(29+posx-14,posy+12,29+posx-14,posy+23,color);
+	LCD_DrawLine(29+posx-15,posy+1,29+posx-15,posy+23,color);
+	LCD_DrawLine(29+posx-16,posy+1,29+posx-16,posy+25,color);
+	LCD_DrawLine(29+posx-17,posy+0,29+posx-17,posy+25,color);
+	LCD_DrawLine(29+posx-18,posy+0,29+posx-18,posy+26,color);
+	LCD_DrawLine(29+posx-19,posy+0,29+posx-19,posy+28,color);
+	LCD_DrawLine(29+posx-20,posy+0,29+posx-20,posy+28,color);
+
+	// Tête
+	LCD_DrawLine(29+posx-19,posy+3,29+posx-19,posy+4,COLOR_BACKGROUND); //oeil
+	LCD_DrawLine(29+posx-20,posy+3,29+posx-20,posy+4,COLOR_BACKGROUND); //oeil
+	LCD_DrawLine(29+posx-21,posy+0,29+posx-21,posy+20,color);
+	LCD_DrawLine(29+posx-22,posy+0,29+posx-22,posy+10,color);
+	LCD_DrawLine(29+posx-23,posy+0,29+posx-23,posy+10,color);
+	LCD_DrawLine(29+posx-24,posy+0,29+posx-24,posy+7,color);
+	LCD_DrawLine(29+posx-25,posy+0,29+posx-25,posy+7,color);
+	LCD_DrawLine(29+posx-26,posy+0,29+posx-26,posy+7,color);
+	LCD_DrawLine(29+posx-27,posy+1,29+posx-27,posy+7,color);
+	LCD_DrawLine(29+posx-28,posy+1,29+posx-28,posy+7,color);
+	LCD_DrawLine(29+posx-29,posy+1,29+posx-29,posy+7,color);
+	
+	// Bouche
+	LCD_DrawLine(29+posx-24,posy+9,29+posx-24,posy+10,color);
+	LCD_DrawLine(29+posx-25,posy+9,29+posx-25,posy+10,color);
+	LCD_DrawLine(29+posx-26,posy+9,29+posx-26,posy+10,color);
+	LCD_DrawLine(29+posx-27,posy+9,29+posx-27,posy+10,color);
+	
+	//Pied
+	LCD_DrawLine(29+posx-11,posy+28,29+posx-11,posy+29,color);
+	LCD_DrawLine(29+posx-12,posy+28,29+posx-12,posy+29,color);
+	LCD_DrawLine(29+posx-21,posy+27,29+posx-19,posy+28,color);
+	LCD_DrawLine(29+posx-22,posy+27,29+posx-20,posy+28,color);
+	
+	//Bras
+	LCD_DrawLine(29+posx-22,posy+15,29+posx-22,posy+16,color);
+	LCD_DrawLine(29+posx-23,posy+15,29+posx-23,posy+17,color);
+	LCD_DrawLine(29+posx-24,posy+15,29+posx-24,posy+17,color);
+	
+}
+
 
 void moveAvatar(struct Avatar *avatar){	
 	
@@ -185,28 +218,22 @@ void moveObstacle(struct Obstacle *obstacle){
 				
 				obstacle->posx = 330;
 				obstacle->posy = posdinoRecu;
-				obstacle->length = 30*(2);
-				obstacle->width = 15*(5+2);
 				
-				//Puisque flag_go = 1, idRecu > 0, on determine l'obstacle à envoyer
-				switch (idRecu) {
-					case 1: obstacle->color=BLUE; break;
-					case 2: obstacle->color=RED; break;
-					case 3: obstacle->color=GREEN; break;
-					case 4: obstacle->color=CYAN; break;
-					case 5: obstacle->color=MAGENTA; break;
-					case 6: obstacle->color=YELLOW; break;
-					case 7: obstacle->color=ORANGE; break;
-				}
+				
+				//Puisque flag_go = 1, idRecu > 0, on envoie
+				obstacle->idTroupe = idRecu;
+				drawDino(obstacle);
+				
 				//On reset, sait-on jamais
 				posdinoRecu = 0;
+
 				idRecu = 0;
 			}
 		}
 
 		obstacle->posx -= 15; //15 est la largeur minimale d'un obstacle
 		LCD_FillRect(obstacle->posx+15,obstacle->posy,obstacle->width, obstacle->length,BLACK);
-		LCD_FillRect(obstacle->posx,obstacle->posy,obstacle->width, obstacle->length,obstacle->color);
+		drawDino(obstacle);
 		
 		obstacle->counter++;
 }
